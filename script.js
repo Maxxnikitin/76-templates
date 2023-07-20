0cc645938d09dd9a38aa90308ed7d6e1653f2ce1
Maxx Nikitin <maxxnikitin90@yandex.ru>
2023-07-20 23:41:16 +0700

    feat: add createElement insert

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const items = [
   { text: "Полить цветы" },
   { text: "Пройти туториал по Реакту" },
   { text: "Сделать фронтенд для своего проекта" },
-  // { text: `<img src='e' onerror='alert("Ваш сайт взломан!")'` },
+  { text: `<img src='e' onerror='alert("Ваш сайт взломан!")'` },
 ];
 
 const container = document.querySelector(".list");
@@ -11,8 +11,27 @@ const container = document.querySelector(".list");
 const render = () => {
   items.forEach((item) => {
     // container.innerHTML += createElByInner(item);
-    container.insertAdjacentHTML("beforeend", createElByInner(item));
+    // container.insertAdjacentHTML("beforeend", createElByInner(item));
+    container.append(createElByCreateEl(item));
   });
+};
+
+const createElByCreateEl = (data) => {
+  const el = document.createElement("li");
+  el.classList.add("list__item");
+
+  const span = document.createElement("span");
+  span.classList.add("item__text");
+  span.textContent = data.text;
+
+  const editBtn = document.createElement("img");
+  editBtn.classList.add("edit");
+  editBtn.src = "images/Edit.png";
+  editBtn.alt = "Редактировать";
+
+  el.append(span, editBtn);
+
+  return el;
 };
 
 const createElByInner = (data) => {
